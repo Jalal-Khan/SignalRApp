0756bcc75cc1142a1ef7ea2963692510a6ddb3ba
jalal-khan <jalalusafxai@gmail.com>
2023-07-06 16:23:59 +0500

    init

---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace WPFClient;
+
+public partial class MainWindow : Window
+{
+    HubConnection connection;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7252/chathub")
+            .WithAutomaticReconnect()
+            .Build();
+
+        connection.Reconnecting += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Attempting to reconnect...";
+                messages.Items.Add(newMessage);
+            });
+        };
+    }
+}
