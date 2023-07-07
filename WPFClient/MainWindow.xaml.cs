bf7bb35c7fe8a3191a8469ea6b3e2225614e32ee
jalal-khan <jalalusafxai@gmail.com>
2023-07-07 15:35:09 +0500

    init

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -25,6 +25,67 @@ public partial class MainWindow : Window
                 var newMessage = "Attempting to reconnect...";
                 messages.Items.Add(newMessage);
             });
+
+            return Task.CompletedTask;
         };
+
+        connection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Reconnected to the server";
+                messages.Items.Add(newMessage);
+            });
+            return Task.CompletedTask;
+        };
+
+        connection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Connection closed";
+                messages.Items.Add(newMessage);
+                openConnetion.IsEnabled= true;
+                sendMessage.IsEnabled= false;
+            });
+            return Task.CompletedTask;
+        };
+    }
+
+    private async void openConnetion_Click(object sender, RoutedEventArgs e)
+    {
+        connection.On<string, string>("RecieveMessage", (user, message) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = $"{user}: {message}";
+                messages.Items.Add(newMessage);
+            });
+        });
+
+        try
+        {
+            await connection.StartAsync();
+            messages.Items.Add("Connection Started");
+            openConnetion.IsEnabled = false;
+            sendMessage.IsEnabled = true;
+        }
+        catch(Exception ex) 
+        {
+            messages.Items.Add(ex.Message);
+        }
+    }
+
+    private async void sendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessage",
+                "WPF Client", messageInput.Text);
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add(ex.Message);
+        }
     }
 }
